b71142e431f5ce7362850e27c564766acb362d60
Mose Mueller <mosemueller@gmail.com>
2023-04-28 15:45:01 +0200

    Moving firewall-response check into status_codes.py

--- certificate_uploader.py
+++ certificate_uploader.py
@@ -1,5 +1,4 @@
 import os
-import re
 from pathlib import Path
 
 import requests
@@ -7,10 +6,8 @@ from dotenv import load_dotenv
 from loguru import logger
 
 from status_codes import (
-    CERTIFICATE_ADDED_SUCCESSFULLY,
-    CERTIFICATE_COULD_NOT_BE_GENERATED,
     CERTIFICATE_NOT_FOUND,
-    CERTIFICATE_UPDATED_SUCCESSFULLY,
+    check_firewall_response_and_return_status_code,
 )
 
 # Load environment variables from the .env file
@@ -23,7 +20,7 @@ FIREWALL_DOMAIN_AND_PORT = os.getenv("FIREWALL_DOMAIN_AND_PORT")
 
 # Read certificate-specific information from environment variables
 CERTIFICATE_PATH = os.getenv("CERTIFICATE_PATH")
-CERTIFICATE_NAME = os.getenv("CERTIFICATE_NAME")
+CERTIFICATE_NAME = os.getenv("CERTIFICATE_NAME", "default_certificate")
 CERTIFICATE_PWD = os.getenv("CERTIFICATE_PWD")
 
 # Evaluates to `True` if set to True (case-insensitive) or 1, else False
@@ -37,78 +34,10 @@ VERIFY_SSL_CERTIFICATE = os.getenv(
 # Construct the firewall API URL
 FIREWALL_API_URL = f"https://{FIREWALL_DOMAIN_AND_PORT}/webconsole/APIController"
 
-status_messages = {
-    "update": {
-        CERTIFICATE_NOT_FOUND: f"Certificate with name '{CERTIFICATE_NAME}' does not exist yet.",
-        CERTIFICATE_UPDATED_SUCCESSFULLY: f"Successfully updated certificate with name '{CERTIFICATE_NAME}'.",
-    },
-    "add": {
-        CERTIFICATE_COULD_NOT_BE_GENERATED: f"Certificate with name '{CERTIFICATE_NAME}' could not be generated. Did you pass the correct certificate path?",
-        CERTIFICATE_ADDED_SUCCESSFULLY: f"Successfully added certificate with name '{CERTIFICATE_NAME}'.",
-    },
-}
-
 cert_path = Path(f"{CERTIFICATE_PATH}")
 cert_file = {
     "file": (cert_path.name, cert_path.read_text(), "text/plain"),
 }
-
-
-def check_firewall_response_return_status_code(
-    update_or_add: str, response: requests.Response
-) -> int:
-    """Check the response from the firewall API, log appropriate messages based on the
-    response, and return the status code.
-
-    This function extracts the authentication status and status code from the firewall
-    API's response. If the authentication status indicates a failure, it logs an error
-    message. If a status code is present, it logs a message based on the action
-    (update or add) and the status code. If no status code is found, it logs a warning.
-
-    Args:
-        update_or_add: The action performed on the certificate ("update" or "add").
-        response: The Response object returned by the requests library after
-        sending a request to the firewall API.
-
-    Returns:
-        The status code returned by the firewall API, or -1 if authentication failed or
-        no status code was found.
-    """
-
-    # Parse the response and extract ...
-    # ... the authentication status
-    authentication_status_pattern = r"<status>Authentication (.*)</status>"
-    match = re.search(authentication_status_pattern, response.text)
-    if match:
-        authentication_status = match.group(1)
-        if authentication_status == "Failure":
-            logger.error(
-                "Could not authenticate with provided credentials. Please update admin "
-                "username or password."
-            )
-            return -1
-
-    # ... the status code
-    status_code_pattern = r'<Status code="(\d+)">'
-    match = re.search(status_code_pattern, response.text)
-
-    if match:
-        status_code = int(match.group(1))
-
-        log_message = status_messages.get(update_or_add, {}).get(status_code)
-        log_level = (
-            logger.error
-            if (update_or_add, status_code)
-            == ("add", CERTIFICATE_COULD_NOT_BE_GENERATED)
-            else logger.info
-        )
-        log_level(log_message) if log_message else logger.warning(
-            f"Unexpected status_code ({status_code}) encountered."
-        )
-    else:
-        logger.warning(f"No status code found: {response.text}.")
-        status_code = -1
-    return status_code
 
 
 def upload_certificate(update_or_add: str) -> int:
@@ -151,8 +80,8 @@ def upload_certificate(update_or_add: str) -> int:
     )
     logger.debug(response.text)
 
-    status_code = check_firewall_response_return_status_code(
-        update_or_add=update_or_add, response=response
+    status_code = check_firewall_response_and_return_status_code(
+        update_or_add=update_or_add, response=response, cert_name=CERTIFICATE_NAME
     )
 
     return status_code
--- status_codes.py
+++ status_codes.py
@@ -1,6 +1,79 @@
+import re
+
+import requests
+from loguru import logger
+
 # Firewall API status codes
 CERTIFICATE_UPDATED_SUCCESSFULLY = 200
 CERTIFICATE_ADDED_SUCCESSFULLY = 200
 CERTIFICATE_NOT_FOUND = 500
 CERTIFICATE_COULD_NOT_BE_GENERATED = 500
 CERTIFICATE_ALREADY_EXISTS = 502
+
+
+def check_firewall_response_and_return_status_code(
+    update_or_add: str, response: requests.Response, cert_name: str
+) -> int:
+    """Check the response from the firewall API, log appropriate messages based on the
+    response, and return the status code.
+
+    This function extracts the authentication status and status code from the firewall
+    API's response. If the authentication status indicates a failure, it logs an error
+    message. If a status code is present, it logs a message based on the action
+    (update or add) and the status code. If no status code is found, it logs a warning.
+
+    Args:
+        update_or_add: The action performed on the certificate ("update" or "add").
+        response: The Response object returned by the requests library after
+        sending a request to the firewall API.
+
+    Returns:
+        The status code returned by the firewall API, or -1 if authentication failed or
+        no status code was found.
+    """
+
+    status_messages = {
+        "update": {
+            CERTIFICATE_NOT_FOUND: f"Certificate with name '{cert_name}' does not exist yet.",
+            CERTIFICATE_UPDATED_SUCCESSFULLY: f"Successfully updated certificate with name '{cert_name}'.",
+        },
+        "add": {
+            CERTIFICATE_COULD_NOT_BE_GENERATED: f"Certificate with name '{cert_name}' could not be generated. Did you pass the correct certificate path?",
+            CERTIFICATE_ADDED_SUCCESSFULLY: f"Successfully added certificate with name '{cert_name}'.",
+        },
+    }
+
+    # Parse the response and extract ...
+    # ... the authentication status
+    authentication_status_pattern = r"<status>Authentication (.*)</status>"
+    match = re.search(authentication_status_pattern, response.text)
+    if match:
+        authentication_status = match.group(1)
+        if authentication_status == "Failure":
+            logger.error(
+                "Could not authenticate with provided credentials. Please update admin "
+                "username or password."
+            )
+            return -1
+
+    # ... the status code
+    status_code_pattern = r'<Status code="(\d+)">'
+    match = re.search(status_code_pattern, response.text)
+
+    if match:
+        status_code = int(match.group(1))
+
+        log_message = status_messages.get(update_or_add, {}).get(status_code)
+        log_level = (
+            logger.error
+            if (update_or_add, status_code)
+            == ("add", CERTIFICATE_COULD_NOT_BE_GENERATED)
+            else logger.info
+        )
+        log_level(log_message) if log_message else logger.warning(
+            f"Unexpected status_code ({status_code}) encountered."
+        )
+    else:
+        logger.warning(f"No status code found: {response.text}.")
+        status_code = -1
+    return status_code
