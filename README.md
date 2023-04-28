4c747c00e14a38e44584f56483ebe7a6c554050a
Mose Mueller <mosemueller@gmail.com>
2023-04-28 15:45:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Sophos Firewall Certificate Uploader
+
+This repository offers a Python script that uploads or updates a certificate on a sophos firewall by interacting with its API.
+It reads necessary information, such as firewall credentials and certificate details, from environment variables and sends a POST request to the firewall API to either update or add the specified certificate.
+
+## Prerequisites
+1. Linux machine
+2. Python 3.9 or later
+3. [`poetry`](https://python-poetry.org/) 1.4 or later
+
+## Installation
+1. Clone the repository
+    ```bash
+    git clone https://gitlab.phys.ethz.ch/tiqi-projects/qchub/network-services/sophos-firewall-certificate-uploader.git
+    ```
+
+2. Install the required dependencies with `poetry`
+    ```bash
+    poetry install
+    ```
+
+## Configuration
+Create an `.env` file in the project's root directory with the following environment variables:
+```ini
+FIREWALL_API_ADMIN=...
+FIREWALL_API_ADMIN_PWD=...
+FIREWALL_DOMAIN_AND_PORT=...
+CERTIFICATE_PATH=...
+CERTIFICATE_NAME=...
+CERTIFICATE_PWD=... (optional)
+VERIFY_SSL_CERTIFICATE=... (optional, default is False)
+```
+Replace the placeholders with your actual values.
+- `FIREWALL_API_ADMIN`: your firewall admin username
+- `FIREWALL_API_ADMIN_PWD`: your firewall admin password
+- `FIREWALL_DOMAIN_AND_PORT`: your firewall domain and port
+  - e.g. `firewall.example.com:4444` or `10.2.0.1:4444`
+- `CERTIFICATE_PATH`: absolute path to your certificate file
+- `CERTIFICATE_NAME`: name of the certificate on the firewall
+- `CERTIFICATE_PWD`: certificate passphrase or preshared key (optional)
+- `VERIFY_SSL_CERTIFICATE`: set to True or 1 to enable SSL certificate verification for the API request
+
+**Note** that using SSL certificate verification is strongly advised as you are passing passwords in the http request.
+
+## Usage
+
+After configuring the .env file, run the script as follows:
+```bash
+[sudo] .venv/bin/python certificate_uploader.py
+```
+Depending on what user you execute the command with, you might need to add `sudo` if the path of the certificate need elevated privileges.
+
+The script will first try to update the certificate with the specified name on the firewall. If the certificate name is not defined yet, the script will add the certificate to the firewall.
+
+The `loguru` logger is used to log important events, errors, and debugging information. You can customize the logger configuration as needed.
---
+++ certificate_uploader.py
@@ -0,0 +1,96 @@
+import re
+import requests
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from loguru import logger
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Read firewall-specific information from environment variables
+FIREWALL_API_ADMIN = os.getenv("FIREWALL_API_ADMIN")
+FIREWALL_API_ADMIN_PWD = os.getenv("FIREWALL_API_ADMIN_PWD")
+# like: "10.2.0.1:4444" or "firewall.something.ch:4444"
+FIREWALL_DOMAIN_AND_PORT = os.getenv("FIREWALL_DOMAIN_AND_PORT")
+
+# Read certificate-specific information from environment variables
+# Absolute path of the certificate to be copied
+CERTIFICATE_PATH = os.getenv("CERTIFICATE_PATH")
+# Name of the certificate to be stored in the firewall (in certificate form)
+CERTIFICATE_NAME = os.getenv("CERTIFICATE_NAME")
+# Passphrase or preshared key (in certificate form). Optional.
+CERTIFICATE_PWD = os.getenv("CERTIFICATE_PWD")
+
+# Evaluates to `True` if set to True (case-insensitive) or 1, else False
+VERIFY_SSL_CERTIFICATE = os.getenv("VERIFY_SSL_CERTIFICATE", default=False).lower() in (
+    "true",
+    "1",
+)
+
+# Construct the firewall API URL
+FIREWALL_API_URL = f"https://{FIREWALL_DOMAIN_AND_PORT}/webconsole/APIController"
+CERTIFICATE_NOT_FOUND = 500
+
+cert_path = Path(f"{CERTIFICATE_PATH}")
+cert_file = {
+    "file": (cert_path.name, cert_path.read_text(), "text/plain"),
+}
+
+
+def upload_certificate(update_or_add: str) -> int:
+    """Uploads or updates a certificate to the firewall by sending a POST request to the
+    firewall API.
+
+    Args:
+        update_or_add: The action to perform on the certificate ("update" or "add").
+
+    Returns:
+        The status code returned by the firewall API.
+    """
+
+    update_certificate_xml = f"""
+    <Request>
+        <Login>
+            <Username>{FIREWALL_API_ADMIN}</Username>
+            <Password>{FIREWALL_API_ADMIN_PWD}</Password>
+        </Login>
+        <Set operation='{update_or_add}'>
+            <Certificate>
+                <Action>UploadCertificate</Action>
+                <Name>{CERTIFICATE_NAME}</Name>
+                <Password>{CERTIFICATE_PWD if CERTIFICATE_PWD is not None else ""}</Password>
+                <CertificateFormat>pem</CertificateFormat>
+                <CertificateFile>{cert_path.name}</CertificateFile>
+                <PrivateKeyFile></PrivateKeyFile>
+            </Certificate>
+        </Set>
+    </Request>
+    """
+
+    # Send a POST request to the firewall API with the XML string and certificate file
+    data = {"reqxml": update_certificate_xml}
+    response = requests.post(
+        FIREWALL_API_URL, data=data, files=cert_file, verify=VERIFY_SSL_CERTIFICATE
+    )
+    logger.debug(response.text)
+
+    # Parse the response and extract the status code
+    pattern = r'<Status code="(\d+)">'
+    match = re.search(pattern, response.text)
+
+    if match:
+        status_code = int(match.group(1))
+    else:
+        raise Exception("API call did not return a response!")
+
+    return status_code
+
+
+if __name__ == "__main__":
+    # Update the certificate with given name on the firewall
+    status_code = upload_certificate("update")
+
+    # If the certificate name is not defined yet, add it
+    if status_code == CERTIFICATE_NOT_FOUND:
+        status_code = upload_certificate("add")
